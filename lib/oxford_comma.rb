950a13e1d3f2e62759d1beb4bcaa91a2fb0e4d9b
layla dantas <laylatdantas@gmail.com>
2019-05-21 05:13:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,13 +3,11 @@ def oxford_comma(array)
    array.join
    
    
- elsif 
- array.length == 2
+ elsif array.length == 2
   array.join(" and ")
   
   
- elsif 
- array.length == 3 
+ elsif array.length == 3 
   array.insert(2, " and ") 
   array.join(",")
   
